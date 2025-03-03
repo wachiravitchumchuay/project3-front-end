9b2b56d21ac0a21362dd05b88506bfa09c0c989d
pinkjo8825 <83690209+pinkjo8825@users.noreply.github.com>
2025-03-03 15:27:07 +0700

    table home

--- src/pages/TableHome.jsx
+++ src/pages/TableHome.jsx
@@ -12,9 +12,15 @@ const TableHome = ({ setActivePage }) => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <div>        
-        <Button onClick={() => setActivePage('TablePage')}>Go to Table Page</Button>
-      </div>
+<div className="bg-green-3 flex flex-col justify-center items-center">
+  <div className="pt-18 flex flex-col justify-center items-center text-center">
+    <p className="font-head text-6xl">Recommender</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, ullam!</p>
+  </div>
+  <div className="p-6">
+    <Button onClick={() => setActivePage("TablePage")}>Start!</Button>
+  </div>
+</div>
     </motion.div>
   );
 };
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -22,29 +22,33 @@ const TablePage = ({ setActivePage }) => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <h1>TablePage Page</h1>
-      <Button onClick={() => setActivePage('TableHome')}>Go to Table Home</Button>
 
-      <FormComponent />
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <div className="bg-green-3">
+        <Button onClick={() => setActivePage("TableHome")}>
+          Go to Table Home
+        </Button>
+
+        <FormComponent />
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </motion.div>
   );
 };
